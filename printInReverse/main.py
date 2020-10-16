07444e8e30c48597ea906e639e93b3be5aeb2cc8
Geek_Anna <anna.tariziyan1@gmail.com>
2020-10-16 14:43:53 +0330

    print the given linkedlist in revers

---
+++ printInReverse/main.py
@@ -0,0 +1,81 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+        self.tail = node
+
+
+def print_singly_linked_list(node, sep):
+    while node:
+        print(node.data, end='')
+
+        node = node.next
+
+        if node:
+            print(sep, end='')
+
+
+# Complete the reversePrint function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def reversePrint(head):
+    p1 = head
+    p2 = p1.next
+    p3 = p2.next
+
+    p2.next = p1
+    p1.next = None
+    while p3 is not None:
+        p1 = p2
+        p2 = p3
+        p3 = p3.next
+        p2.next = p1
+    while p2 is not None:
+        print(p2.data)
+        p2 = p2.next
+
+
+if __name__ == '__main__':
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        llist_count = int(input())
+
+        llist = SinglyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        reversePrint(llist.head)
